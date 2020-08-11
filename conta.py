7e8999255ded7c7c557c4c0646e3e40a96b60338
Rafael Nascimento <61482757+rafaelvalado@users.noreply.github.com>
2020-08-11 04:40:12 -0300

    Create conta.py

---
+++ conta.py
@@ -0,0 +1,24 @@
+from collections import named tuple
+# criando uma classe simples usando namedtuple
+Cliente = namedtuple('Cliente', 'nome sobrenome cpf')
+
+class Conta:
+    def __init__(self, numero, titular, saldo, limite=1000):
+        self._numero = numero
+        self._titular = Cliente()
+        self._saldo = saldo
+        self._limite = limite
+
+    
+    def depositar(self, valor):
+        if valor < 0:
+            return False
+        else:
+            self._saldo += valor
+
+    
+    def sacar(self, valor):
+        if valor < self._saldo:
+            return False
+        else:
+           self._saldo -= valor
