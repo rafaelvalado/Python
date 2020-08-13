198a9ace2dbb0b84c3bdb3aff160a3c9d2d3f2e2
Rafael Nascimento <61482757+rafaelvalado@users.noreply.github.com>
2020-08-13 15:37:55 -0300

    Update range.py

--- ranges/range.py
+++ ranges/range.py
@@ -8,3 +8,9 @@
  
 for num in range(11):
     print(num)
+
+# froma 02 - range(valor_de_início, valor_de_parada); obs-: valor_de_parada não inclusivo-(início_dado_pelo_usuário, passo 1 em 1).
+# ex02
+ 
+for num in range(2, 11):
+    print(num)
