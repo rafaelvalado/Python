a47e1c2e0fe5616927317056159424bac6acb89a
Rafael Nascimento <61482757+rafaelvalado@users.noreply.github.com>
2020-08-13 15:24:37 -0300

    Create range.py

---
+++ ranges/range.py
@@ -0,0 +1,10 @@
+# Ranges
+""" Ranges são utilizados para gerar sequências numéricas, não de forma aleatória mas sim de maneira especificada. """
+ 
+# formas gerais:
+ 
+# froma 01 - range(valor_de_parada); obs: valor_de_parada não inclusivo(início padrão 0, e passo de 1 em 1)
+# ex01
+ 
+for num in range(11):
+    print(num)
