20a779ddc36a39c36ef7483e00630fe7ffcd5cf0
Rafael Nascimento <61482757+rafaelvalado@users.noreply.github.com>
2020-08-13 18:25:21 -0300

    Update range.py

--- ranges/range.py
+++ ranges/range.py
@@ -14,3 +14,9 @@ for num in range(11):
  
 for num in range(2, 11):
     print(num)
+
+# forma 04 - range(final, parada, passo); obs: parada não inclusivo(início_dado_pelo_usuário, passo_pelo_usuário)
+# ex04
+ 
+for num in range(10, -1, -1): # (-1) -> decrementação; faz uma contagem regressiva. Nesse caso,só ocorre o loop s
+    print(num)
